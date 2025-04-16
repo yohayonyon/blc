ced828c0bdbbae2683b363f3be7332594ad20275
yohayonyon <yochayon@gmail.com>
2025-04-16 14:41:59 +0300

    sh script to add email env vars

---
+++ set_blc_email_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export BLC_SENDER_SMTP_ADDRESS="smtp.gmail.com"
+export BLC_SENDER_SMTP_PORT="587"
+export BLC_SENDER_EMAIL=""
+export BLC_SENDER_EMAIL_PASSWORD=""
+
+echo "Email environment variables set."
