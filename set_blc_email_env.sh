6fa2ef28a352d31eb732c661fbb38ce037d0fee4
yohayonyon <yochayon@gmail.com>
2025-04-16 16:09:19 +0300

    remove old sh

--- set_blc_email_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export BLC_SENDER_SMTP_ADDRESS="smtp.gmail.com"
-export BLC_SENDER_SMTP_PORT="587"
-export BLC_SENDER_EMAIL=""
-export BLC_SENDER_EMAIL_PASSWORD=""
-
-echo "Email environment variables set."
